5b665b18c10cec83fdd20fad368373a1a2c016a5
kumastry <chma19063@g.nihon-u.ac.jp>
2022-07-07 16:03:46 +0900

    first

---
+++ gatsby-config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
+
+
+module.exports = {
+  siteMetadata: {
+    title: `kumastry-blog`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-microcms',
+      options: {
+        apiKey: process.env.API_KEY,
+        serviceId: 'kumastryblog',
+        apis: [{
+          endpoint: 'blogs',
+        }],
+      },
+    },
+    {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "kumastry2212"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
